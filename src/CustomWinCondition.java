465761f85f9baddf0b1002441bfec6cdda6e7ff8
Cathy <catherineyin13@gmail.com>
2025-04-15 17:16:17 -0400

    complete class CustomWinCondition implement interface WinCondition

---
+++ src/CustomWinCondition.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.function.Function;
+
+public class CustomWinCondition implements WinCondition{
+    private Function<List<Movie>, Boolean> condition;
+
+    public CustomWinCondition(Function<List<Movie>, Boolean> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public boolean checkWin(Player player) {
+        return condition.apply(player.getMoviesPlayed());
+    }
+}
