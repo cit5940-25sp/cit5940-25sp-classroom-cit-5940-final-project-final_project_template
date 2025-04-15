32600d3d1ad4c42f9c090498ef504b5cd40c0c67
Cathy <catherineyin13@gmail.com>
2025-04-15 16:34:52 -0400

    create Movie class

---
+++ src/Movie.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Set;
+
+public class Movie {
+    private String title;
+    private String releaseYear;
+    private Set<String> genres;
+    private List<String> actors;
+    private List<String> directors;
+    private List<String> composers;
+    private List<String> writers;
+    private List<String> cinematographers;
+}
