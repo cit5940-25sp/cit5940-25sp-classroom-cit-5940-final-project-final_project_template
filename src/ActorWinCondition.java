ccab390edb2f8500a0c948cd38049df5f8a64a89
Cathy <catherineyin13@gmail.com>
2025-04-15 17:10:40 -0400

    complete class ActorWinCondition implement interface WinCondition

---
+++ src/ActorWinCondition.java
@@ -0,0 +1,17 @@
+public class ActorWinCondition implements WinCondition {
+    private String actor;
+
+    public ActorWinCondition(String actor) {
+        this.actor = actor;
+    }
+
+    @Override
+    public boolean checkWin(Player player) {
+        for (Movie movie : player.getMoviesPlayed()) {
+            if (movie.getActors().contains(actor)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
