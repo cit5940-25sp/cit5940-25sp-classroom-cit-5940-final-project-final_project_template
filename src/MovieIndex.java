77b543c1de6431fb7a1055741225a5f31d092b34
melodyhashemi <mhashemi@seas.upenn.edu>
2025-04-20 21:49:59 -0400

    updated MovieIndex class

--- src/MovieIndex.java
+++ src/MovieIndex.java
@@ -1,40 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // Handles indexing and searching movies (uses map) (model)
 public class MovieIndex implements IMovieIndex {
-    private List<IMovie> movieList;
+    private Map<String, IMovie> indexByTitle = new HashMap<>();
+    private Map<String, Set<IMovie>> indexByContributor = new HashMap<>();
 
-    public MovieIndex() {
-        this.movieList = new ArrayList<>();
-    }
 
     @Override
     public void loadMovies(List<IMovie> movieList) {
-        if (movieList == null) {
-            this.movieList = new ArrayList<>();
-        } else {
-            this.movieList = movieList;
+        for (IMovie movie : movieList) {
+            indexByTitle.put(movie.getTitle().toLowerCase(), movie);
+            for (String person : movie.getAllContributors()) {
+                if (!indexByContributor.containsKey(person.toLowerCase())) {
+                    indexByContributor.put(person.toLowerCase(), new HashSet<>());
+                } else {
+                    indexByContributor.get(person).add(movie);
+                }
+            }
         }
     }
 
     @Override
     public IMovie getMovieByTitle(String title) {
-        return null;
+        return indexByTitle.get(title.toLowerCase());
     }
 
     @Override
     public List<String> autocomplete(String input) {
-        return List.of();
+        String inputLowerCase = input.toLowerCase();
+        List<String> results = new ArrayList<>();
+
+        for (String title : indexByTitle.keySet()) {
+            if (title.startsWith(inputLowerCase)) {
+                results.add(indexByTitle.get(title).getTitle());
+            }
+        }
+        return results;
     }
 
     @Override
     public List<IMovie> getConnectedMovies(IMovie movie) {
-        return List.of();
+        Set<IMovie> connectedMovies = new HashSet<>();
+
+        for (String person : movie.getAllContributors()) {
+            String name = person.toLowerCase();
+
+            if (indexByContributor.containsKey(name)) {
+                connectedMovies.addAll(indexByContributor.get(name));
+            }
+        }
+
+        connectedMovies.remove(movie);
+        return new ArrayList<>(connectedMovies);
     }
 
     @Override
     public boolean containsMovie(String title) {
-        return false;
+        return indexByTitle.containsKey(title.toLowerCase());
     }
 }
