5a12ba48ba7ef0180071c9eb0e6a1786d308114d
Meghana <mmeghana@seas.upenn.edu>
2025-04-27 18:20:36 -0400

    Updated HelloWorld class again

--- test/HelloWorldTest.java
+++ test/HelloWorldTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
 
     @Test
     public void testSayHello() {
+
         assertEquals("Hello, World!", HelloWorld.sayHello());
     }
 
