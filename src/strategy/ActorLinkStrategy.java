a348eb079ab42f14defeb01d74c39894bb477e2c
Gevenal <jianjiayi679@gmail.com>
2025-05-01 14:19:30 -0400

    ActorLinkStrategy updated

--- src/strategy/ActorLinkStrategy.java
+++ src/strategy/ActorLinkStrategy.java
@@ -1,6 +1,7 @@
 package strategy;
 
 import model.Movie;
+import model.Person;
 
 /**
  * Link strategy where two movies are considered connected if they share at least one actor.
@@ -9,13 +10,25 @@ public class ActorLinkStrategy implements ILinkStrategy {
 
     @Override
     public boolean isValidLink(Movie from, Movie to) {
-        // TODO
+        for (Person actor1 : from.getActors()) {
+            for (Person actor2 : to.getActors()) {
+                if (actor1.getName().equals(actor2.getName())) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public String getReason(Movie from, Movie to) {
-        // TODO
-        return null;
+        for (Person actor1 : from.getActors()) {
+            for (Person actor2 : to.getActors()) {
+                if (actor1.getName().equals(actor2.getName())) {
+                    return "Shared actor: " + actor1.getName();
+                }
+            }
+        }
+        return "No shared actors";
     }
 }
