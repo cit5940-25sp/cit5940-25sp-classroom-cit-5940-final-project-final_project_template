5fe4dfaa7e971a01bfc9542298bbb413bf9fd4a7
vanessachung <vanessa137222@gmail.com>
2025-05-01 17:09:55 -0600

    complete game view

--- src/GameView.java
+++ src/GameView.java
@@ -1,4 +1,5 @@
 import java.util.List;
+
 /**
  * Handles rendering game state and displaying information to the user.
  */
@@ -9,8 +10,15 @@ public class GameView {
      *
      * @param state the current game state
      */
-    public void render(GameState state){
-
+    public void render(GameState state) {
+        System.out.println("--- Game State ---");
+        System.out.println("Current Round: " + state.getCurrRound());
+        System.out.println("Current Player: " + state.getCurrentPlayer().getName());
+        System.out.println("Recent History:");
+        for (Movie movie : state.getRecentHistory()) {
+            System.out.println(" - " + movie.getTitle() + " (" + movie.getYear() + ")");
+        }
+        System.out.println();
     }
 
     /**
@@ -18,8 +26,8 @@ public class GameView {
      *
      * @param message the message to display
      */
-    public void displayInfo(String message){
-
+    public void displayInfo(String message) {
+        System.out.println("[INFO] " + message);
     }
 
     /**
@@ -27,7 +35,11 @@ public class GameView {
      *
      * @param suggestions list of suggested movie titles
      */
-    public void showAutocomplete(List<String> suggestions){
-
+    public void showAutocomplete(List<String> suggestions) {
+        System.out.println("Did you mean:");
+        for (String suggestion : suggestions) {
+            System.out.println(" - " + suggestion);
+        }
     }
 }
+
