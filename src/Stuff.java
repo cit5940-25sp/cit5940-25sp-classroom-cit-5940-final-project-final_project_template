26f8b8378739dd38804042fedf7aa9264cf84cfc
danielboyuw <boyuw@seas.upenn.edu>
2025-05-07 19:36:30 -0400

    version 2 of Stuff

--- src/Stuff.java
+++ src/Stuff.java
@@ -1,14 +1,25 @@
-public class Stuff {
+public class Stuff implements Comparable<Stuff>{
     private String name;
     private int id;
+    public String getJob(){
+        return "";
+    }
     public Stuff(String name, int id){
         this.name = name;
-        this.id = id; 
+        this.id = id;
     }
     public String getName(){
-        return name; 
+        return name;
     }
     public int getId(){
-        return id; 
+        return id;
+    }
+    @Override
+    public int compareTo(Stuff o) {
+        return Integer.compare(id, o.id);
+    }
+    @Override
+    public String toString(){
+        return name;
     }
 }
