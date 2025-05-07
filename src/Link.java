6d21477e65d6669a9122aed4295b54e8bb2f8470
YeZou <zouye@seas.upenn.edu>
2025-05-07 15:49:16 -0400

    implement constructor to calculate shared staff between two movies

--- src/Link.java
+++ src/Link.java
@@ -1,7 +1,15 @@
-public class Link {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Link implements Comparable<Link>{
+    //The first movie
     private Movie movie1;
+    //The second movie
     private Movie movie2;
-    private Stuff share;
+    //A list of Stuff that is shared bewteen two
+    private List<Stuff> share;
+    private String connectJob1;
+    private String connectJob2;
 
     boolean isValidLink(){
         return false
