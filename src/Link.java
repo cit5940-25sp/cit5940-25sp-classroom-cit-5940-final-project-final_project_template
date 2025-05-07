80e2975006a9d9b956a0de830e43e30186e7349c
YeZou <zouye@seas.upenn.edu>
2025-05-07 15:56:47 -0400

    implement Link constructor with shared staff initialization and getter for movie1

--- src/Link.java
+++ src/Link.java
@@ -11,7 +11,23 @@ public class Link implements Comparable<Link>{
     private String connectJob1;
     private String connectJob2;
 
-    public
+    public Link(Movie movie 1, Movie movie 2){
+        //initialize the first movie
+        this.movie1 = movie1;
+        // initialize the second
+        this.movie2 = movie2;
+        //initialize the list
+        share = new ArrayList<>();
+        //Calculate
+        calculateShare();
+    }
+
+    public Movie getMovie1() {
+        return movie1;
+    }
+
+
+
     boolean isValidLink(){
         return false
     }
