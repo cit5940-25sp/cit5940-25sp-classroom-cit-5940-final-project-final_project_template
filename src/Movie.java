c86ec261ecfbb80a79e0348917432ad271e5a24d
danielboyuw <boyuw@seas.upenn.edu>
2025-05-07 13:49:06 -0400

    version 2 of Movie

--- src/Movie.java
+++ src/Movie.java
@@ -1,24 +1,31 @@
-public class Movie {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+public class Movie implements Comparable<Movie> {
     private String title;
     private int id;
-    private int year;
-    private Map<Stuff> cast;
-    private Map<Stuff> crew;
-    public Movie(String title, int id, int year){
+    private String date;
+    private List<Genre> genres;
+    private TreeSet<Stuff> stuffs;
+    public Movie(int id){
+        this.title = "";
+        this.id = id;
+        this.date = "";
+    }
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public Movie(String title){
+        this.title = title;
+        this.id = -1;
+        this.date = "";
+    }
+    public Movie(String title, int id, String date){
         this.title = title;
         this.id = id;
-        this.year = year; 
+        this.date = date;
+        genres = new ArrayList<>();
+        stuffs = new TreeSet<>();
     }
-    public String getTitle(){
-        return title; 
-    }
-    public int getID(){
-        return id; 
-    }
-    public int getYear(){
-        return year; 
-    }
-    public toString(){
-        return title + "(" + year + ")";
-    }
-}
