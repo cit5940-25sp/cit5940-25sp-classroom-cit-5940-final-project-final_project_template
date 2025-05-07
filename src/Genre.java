096ddd8d942eaf4d8e00c42778b4c170d2374a14
danielboyuw <boyuw@seas.upenn.edu>
2025-05-07 12:02:40 -0400

    version 2 of Genre

--- src/Genre.java
+++ src/Genre.java
@@ -1,21 +1,30 @@
-public class Genre{
-    private String name;
+public class Genre implements Comparable<Genre>{
     private int id;
+    private String name;
 
     public Genre(String name, int id){
         this.name = name;
         this.id = id;
     }
     public String getName(){
-        return name; 
+        return name;
     }
     public int getId(){
-        return id; 
+        return id;
     }
     public void setName(String name){
-        this.name = name; 
+        this.name = name;
     }
     public void setId(int id){
-        this.id = id; 
+        this.id = id;
+    }
+    @Override
+    public int compareTo(Genre other){
+        return Integer.compare(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
