e0a4e27c0a1c8fb3fe0fb55a0978a002e6c41177
danielboyuw <boyuw@seas.upenn.edu>
2025-05-07 17:18:19 -0400

    version 3 of ParseJson

--- src/ParseJson.java
+++ src/ParseJson.java
@@ -25,3 +25,36 @@ public class ParseJson {
         Genre[] list = gson.fromJson(json, Genre[].class);
         return list;
     }
+
+    /**
+     * Parses a JSON string into an array of Stuff objects representing cast members.
+     *
+     * @param json The JSON string to be parsed.
+     * @return An array of Stuff objects if parsing is successful, null otherwise.
+     */
+    public Stuff[] parseCast(String json){
+        try {
+            Gson gson = new Gson();
+            // Deserialize the JSON string into an array of raw Map objects
+            // Note: This causes a raw type warning; consider parameterizing the Map
+            Map[] map = gson.fromJson(json, Map[].class);
+            Stuff[] stuff = new Stuff[map.length];
+            for (int i = 0; i < map.length; i++) {
+                // Unchecked conversion from raw Map to parameterized Map<String, Object>
+                Map<String, Object> entry = map[i];
+                String name = (String) entry.get("name");
+                String character = (String)entry.get("character");
+                double d = (double)entry.get("id");
+                int id = (int)d ;
+                d = (double) entry.get("order");
+                int order = (int)d;
+                // Create a new Cast object and add it to the array
+                stuff[i] = new Cast(name, id, character,order);
+            }
+            return stuff;
+        } catch (Exception e){
+            // Print the stack trace if an exception occurs during parsing
+            e.printStackTrace();
+        }
+        return null;
+    }
