44a1e8e328927f0c94e5a113002db663dd0a4fcb
YeZou <zouye@seas.upenn.edu>
2025-05-07 21:01:43 -0400

    comments

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,8 @@
 public class Player {
     private String name;
     private Links links;
-    
+
+    // Create a new player with the given name and an empty link path
     public Player(String name) {
         this.name = name;
         links = new Links();
