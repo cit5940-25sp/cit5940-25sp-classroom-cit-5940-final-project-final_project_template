582b1d2f64ac9957788bf4a4c1fcade2c4651746
danielboyuw <boyuw@seas.upenn.edu>
2025-05-07 21:17:32 -0400

    version 1 of Crew

---
+++ src/Crew.java
@@ -0,0 +1,19 @@
+public class Crew extends Stuff{
+    private String job;
+    public Crew(String name, int id, String job){
+        super(name, id);
+        this.job = job;
+    }
+    public String getJob(){
+        return job;
+    }
+    public void setJob(String job){
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        String str = getJob() + super.toString();
+        return str;
+    }
+}
