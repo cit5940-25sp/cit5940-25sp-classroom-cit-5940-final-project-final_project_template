304850a8724dac0cd293f8b50fb8dc0bba56e356
danielboyuw <boyuw@seas.upenn.edu>
2025-05-07 21:37:33 -0400

    version 1 of Cast

---
+++ src/Cast.java
@@ -0,0 +1,29 @@
+public class Cast extends Stuff{
+    private String character;
+    private int order;
+    public Cast(String name, int id, String character, int order){
+        super(name, id);
+        this.character = character;
+        this.order = order;
+    }
+
+    public String getCharacter(){
+        return character;
+    }
+    public void setCharacter(String character){
+        this.character = character;
+    }
+    public int getOrder(){
+        return order;
+    }
+
+    @Override
+    public String toString() {
+        String str = "Cast:" + super.toString();
+        return str;
+    }
+    @Override
+    public String getJob() {
+        return "Actor";
+    }
+}
