a9f467175433fcb7bbc1fb68f29b0d192d5b3e09
YeZou <zouye@seas.upenn.edu>
2025-05-08 21:47:59 -0400

    game test started

---
+++ test/GameTest.java
@@ -0,0 +1,48 @@
+package test;
+import AutoComplete.Autocomplete;
+import AutoComplete.Node;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+
+    @Test
+    public void testAddWord() {
+        Autocomplete autocomplete = new Autocomplete(5);
+        String word = "123";
+        long weight = 123;
+        autocomplete.addWord(word, weight);
+        Node node = autocomplete.getSubTrie(word);
+        assertNull(node);
+        word = "abc";
+        weight = 123;
+        autocomplete.addWord(word, weight);
+        node = autocomplete.getSubTrie(word);
+        assertNotNull(node);
+        assertEquals(weight, node.getTerm().getWeight());
+        autocomplete.addWord("abcd", 101);
+        autocomplete.addWord("abcde", 101);
+        int count = autocomplete.countPrefixes(word);
+        assertEquals(3, count);
+        count = autocomplete.countPrefixes("abcde");
+        assertEquals(1, count);
+    }
+
+
+    @Test
+    public void testBuildTrieAndSuggestions() {
+        Autocomplete autocomplete = new Autocomplete(5);
+        String filename = "pokemon.txt";
+        int k = 3;
+        //Node root = autocomplete.buildTrie(filename, k);
+        //List<ITerm> suggestions = autocomplete.getSuggestions("b");
+        //assertEquals(5, suggestions.size());
+        //assertEquals("braixen", suggestions.get(0).getTerm());
+        //assertEquals("braviary", suggestions.get(1).getTerm());
+        //assertEquals("breloom", suggestions.get(2).getTerm());
+    }
+
+
+}
