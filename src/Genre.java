f3f307982e6f7cc5b0495afefbfbb5840f56f1ba
danielboyuw <boyuw@seas.upenn.edu>
2025-05-09 21:25:13 -0400

    Update Genre

--- src/Genre.java
+++ src/Genre.java
@@ -1,31 +1,63 @@
+/**
+ * Represents a genre with an ID and a name.
+ * This class implements the Comparable interface to allow sorting of Genre objects.
+ */
 public class Genre implements Comparable<Genre>{
+    // Unique identifier for the genre
     private int id;
+    // Name of the genre
     private String name;
 
+    /**
+     * Constructs a new Genre object with the specified name and ID.
+     *
+     * @param name the name of the genre
+     * @param id the unique identifier for the genre
+     */
     public Genre(String name, int id){
         this.name = name;
         this.id = id;
     }
+
+    /**
+     * Retrieves the name of the genre.
+     *
+     * @return the name of the genre
+     */
     public String getName(){
         return name;
     }
-    public int getId(){
-        return id;
-    }
+
+    /**
+     * Sets the name of the genre.
+     *
+     * @param name the new name of the genre
+     */
     public void setName(String name){
         this.name = name;
     }
-    public void setId(int id){
-        this.id = id;
-    }
+
+    /**
+     * Compares this genre with another genre for order.
+     * Returns a negative integer, zero, or a positive integer as this genre's ID
+     * is less than, equal to, or greater than the specified genre's ID.
+     *
+     * @param other the genre to be compared
+     * @return a negative integer, zero, or a positive integer as this genre's ID
+     *         is less than, equal to, or greater than the specified genre's ID
+     */
     @Override
     public int compareTo(Genre other){
         return Integer.compare(this.id, other.id);
     }
 
+    /**
+     * Returns a string representation of the genre, which is its name.
+     *
+     * @return the name of the genre
+     */
     @Override
     public String toString() {
         return name;
     }
 }
-*
