f087e4d272cb7ece08dd1ce318cb20ac0cc82d67
Meghana <mmeghana@seas.upenn.edu>
2025-05-12 12:40:20 -0400

    Added tests in MoviesTest.java

--- test/MoviesTest.java
+++ test/MoviesTest.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 import java.util.*;
+import static org.junit.Assert.*;
 
 public class MoviesTest {
 
@@ -33,13 +33,31 @@ public class MoviesTest {
         String movie1 = "Crazy, Stupid, Love. (2011)";
         String movie2 = "Superbad (2007)";
         List<String> actual = movies.getConnection(movie1, movie2);
-        List<String> expected = Arrays.asList("Emma Stone", "Charlie Hartsock");
+        List<String> expected = new ArrayList<>(Arrays.asList("Emma Stone", "Charlie Hartsock"));
         assertEquals(expected, actual);
     }
 
     @Test
+    public void createAutocompleteFileTest() {
+        movies = new Movies("tmdb_data.txt");
+        Collection<String> output = movies.createAutocompleteFile(movies.getAllTitles());
+        assertNotNull(output);
+        assertFalse(output.isEmpty());
+        assertTrue(output.contains("0\tCrazy, Stupid, Love. (2011)"));
+    }
+
+    @Test
+    public void getMovieGenresEmptyMovieTest() {
+        movies = new Movies("tmdb_data.txt");
+        List<String> genres = movies.getMovieGenres("Nonexistent Movie (2000)");
+        assertTrue(genres.isEmpty());
+    }
+
+
+    @Test
     public void invalidConnectionTest() {
         movies = new Movies("tmdb_data.txt");
+        // movies have no connections
         String movie1 = "Crazy, Stupid, Love. (2011)";
         String movie2 = "Beetlejuice (1988)";
         List<String> actual = movies.getConnection(movie1, movie2);
@@ -55,7 +73,7 @@ public class MoviesTest {
     @Test
     public void getAllGenresTest() {
         movies = new Movies("tmdb_data.txt");
-        TreeMap<String, List<String>> genres = movies.getAllGenres();
+        Map<String, Integer> genres = movies.getAllGenres();
         assertNotNull(genres);
         assertTrue(genres.size() > 0);
     }
@@ -71,12 +89,11 @@ public class MoviesTest {
     @Test
     public void getMovieGenresTest() {
         movies = new Movies("tmdb_data.txt");
-        List<String> genres = movies.getMovieGenres("The Dark Knight Rises (2012)");
+        List<String> genres = movies.getMovieGenres("Superbad (2007)");
         assertTrue(genres.contains("Comedy"));
 
         List<String> missingGenres = movies.getMovieGenres("Nonexistent Movie (2000)");
         assertTrue(missingGenres.isEmpty());
     }
+
 }
-
-
